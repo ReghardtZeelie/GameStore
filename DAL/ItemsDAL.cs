2a897c486958d0e41a0cca423b146045751d9d20
Reghardt Zeelie <Reghardt0420@hotmail.com>
2023-12-06 07:54:21 +0200

    BaseCommit

---
+++ DAL/ItemsDAL.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ItemsDAL
+    {
+        private readonly IConfiguration _configuration;
+
+        public ItemsDAL(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+    }
+}
---
+++ Models/UsersModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Models
+{
+    public class UsersModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime Age { get; set; }
+        public int CartID { get; set; }
+
+    }
+}
